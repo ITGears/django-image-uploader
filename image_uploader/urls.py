cf2e53b5a88eea1065062784628f2ba7df9860d0
Hoongun <hoongun@gmail.com>
2013-04-24 14:37:25 +0400

    Removed import urls.defaults

--- image_uploader/urls.py
+++ image_uploader/urls.py
@@ -1,6 +1,4 @@
-from django.conf.urls.defaults import patterns, url
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls import patterns, url
 
 from views import Upload
 
